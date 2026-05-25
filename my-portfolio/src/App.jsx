0ea72fe2b8afcc487f1cd56259d1394fead7de90
Raunak <paraterr@rknec.edu>
2026-05-25 16:24:35 +0530

    Portfolio

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+  const name = "Raunak Parate";
+  const course = "B.Tech Computer Science and Engineering";
+  const city = "Nagpur";
+  return (
+    <div>
+      <h1>My name is {name}</h1>
+      <p>My course is {course}</p>
+      <p>I live in {city}</p>
+    </div>
+  )
+}
+
+export default App
